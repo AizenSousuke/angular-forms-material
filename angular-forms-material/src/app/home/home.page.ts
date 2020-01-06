4dc2b44c57cb13cdd158e195141b8d1b029a0268
AizenSousuke <puteranikdimas@yahoo.com.sg>
2020-01-06 07:35:28 +0000

    Added form

--- angular-forms-material/src/app/home/home.page.ts
+++ angular-forms-material/src/app/home/home.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    storyForm;
 
+    constructor(private formBuilder: FormBuilder) { 
+        this.storyForm = this.formBuilder.group({
+            formArray: this.formBuilder.array([
+                this.formBuilder.group({
+                    image: []
+                }),
+                this.formBuilder.group({
+                    story: []
+                }),
+            ]),
+        });
+        console.log(this.storyForm);
+    }
+
+    onStorySubmit() {
+
+    }
 }
